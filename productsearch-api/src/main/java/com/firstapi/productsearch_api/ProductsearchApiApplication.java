374346ff915f376d23dd450aa8d4fcdd3bd8ee17
aasif-create <aasifmmd12345@gmail.com>
2025-09-13 22:12:27 +0530

    ADDED BASIC API FROM SPRING INITIALIZR

---
+++ productsearch-api/src/main/java/com/firstapi/productsearch_api/ProductsearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.firstapi.productsearch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsearchApiApplication.class, args);
+	}
+
+}
---
+++ productsearch-api/src/test/java/com/firstapi/productsearch_api/ProductsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstapi.productsearch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
