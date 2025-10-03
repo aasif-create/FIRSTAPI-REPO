22533dde52090e9b4c4b7e9846d7f6fc87fedc29
aasif-create <aasifmmd12345@gmail.com>
2025-10-03 21:50:11 +0530

    UPDATED

---
+++ productsearch-api/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO items (name, category, price, rating, image_url, available) VALUES
+('Idly', 'South Indian', 30.0, 4.5, '/images/idly.jpg', true),
+('Masala Dosa', 'South Indian', 70.0, 4.7, '/images/masala_dosa.jpg', true),
+('Sambar Vada', 'South Indian', 45.0, 4.2, '/images/sambar_vada.jpg', true),
+('Chicken Biryani', 'Biryani', 150.0, 4.8, '/images/chicken_biryani.jpg', true),
+('Paneer Butter Masala', 'North Indian', 140.0, 4.6, '/images/paneer_butter_masala.jpg', true),
+('Poori', 'North Indian', 40.0, 4.1, '/images/poori.jpg', true),
+('Rasam Rice', 'South Indian', 60.0, 4.0, '/images/rasam_rice.jpg', true),
+('Hyderabadi Biryani', 'Biryani', 180.0, 4.9, '/images/hyderabadi_biryani.jpg', true),
+('Uttapam', 'South Indian', 80.0, 4.3, '/images/uttapam.jpg', true),
+('Gulab Jamun', 'Dessert', 50.0, 4.4, '/images/gulab_jamun.jpg', true),
+('Parotta with Kurma', 'South Indian', 90.0, 4.5, '/images/parotta.jpg', true),
+('Mutton Biryani', 'Biryani', 200.0, 4.8, '/images/mutton_biryani.jpg', true),
+('Veg Fried Rice', 'Chinese', 120.0, 4.2, '/images/veg_fried_rice.jpg', true),
+('Chicken 65', 'Starter', 110.0, 4.6, '/images/chicken65.jpg', true),
+('Chole Bhature', 'North Indian', 100.0, 4.5, '/images/chole_bhature.jpg', true),
+
